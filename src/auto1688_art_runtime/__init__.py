b34309a2f466061ee825ee2a3b2e6e21309e0ff3
yuanshengchen72-rgb <yuanshengchen72@gmail.com>
2026-08-14 16:29:04 +0800

    Publish Auto1688 Qwen3.5 AWQ runtime

---
+++ src/auto1688_art_runtime/__init__.py
@@ -0,0 +1,7 @@
+from auto1688_art_runtime.runtime import (
+    RuntimePreparationError,
+    check_image,
+    prepare_runtime,
+)
+
+__all__ = ["RuntimePreparationError", "check_image", "prepare_runtime"]
